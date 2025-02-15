7f8469ad7e1cce8391044148e61f24bd29daf595
Rohan Adwankar <39285979+RohanAdwankar@users.noreply.github.com>
2025-02-15 00:03:44 -0800

    added Readme to python package, published to npm

--- fosum-py/setup.py
+++ fosum-py/setup.py
@@ -1,12 +1,19 @@
 from setuptools import setup, find_packages
+import pathlib
+
+here = pathlib.Path(__file__).parent.resolve()
+long_description = (here / "README.md").read_text(encoding="utf-8")
 
 setup(
     name="fosum",
-    version="0.5.0",
-    description="A simple tool that summarizes file folders. Useful for passing context to LLMs",
+    version="0.6.0",
+    description="A simple tool that summarizes file folders. Useful for passing context to LLMs.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
     author="Rohan Adwankar",
     author_email="rohan.adwankar@gmail.com",
     license="MIT",
+    url="https://github.com/RohanAdwankar/utils",
     packages=find_packages(),
     include_package_data=True,
     package_data={
